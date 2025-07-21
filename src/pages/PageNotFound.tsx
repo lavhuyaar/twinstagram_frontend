e20a382b4a890099d1a7992df2c46cd96c48f7fa
Lav <lavhuyaar@gmail.com>
2025-07-21 11:32:09 +0530

    fix: Page Not Found UI fixed

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,6 +1,11 @@
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
-export default PageNotFound
+    <>
+      <main className="p-6 sm:px-[5%] py-10 text-center items-center justify-center h-screen w-full flex flex-col max-w-[1800px]">
+        <h1 className=" text-[80px] md:text-[100px] font-bold">404</h1>
+        <h2 className="text-xl font-medium">Page not found</h2>
+      </main>
+    </>
+  );
+};
+export default PageNotFound;
