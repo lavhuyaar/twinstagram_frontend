7541e06cccbfd66372890c8c2d786700ee3ffe42
Lav <lavhuyaar@gmail.com>
2025-07-22 14:15:53 +0530

    fix: Z-index of Navigation Bar

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -14,7 +14,7 @@ const MainLayout = ({
       <RedirectToLogin />
       <div className="h-screen w-full flex max-w-[1800px] relative">
         <Sidebar className="hidden md:flex overflow-x-auto w-[400px] z-99 overflow-y-auto flex-col bg-surface p-4 gap-2 text-text-primary" />
-        <NavigationBar className="md:hidden flex items-center justify-evenly fixed bottom-0 bg-primary w-full h-[60px]" />
+        <NavigationBar className="md:hidden flex items-center justify-evenly fixed bottom-0 z-[99999] bg-primary w-full h-[60px]" />
         <main
           className={`h-full w-full overflow-y-auto text-text-primary flex flex-col py-10 px-3 sm:p-10 items-center justify-baseline ${
             mainStyling ?? ""
